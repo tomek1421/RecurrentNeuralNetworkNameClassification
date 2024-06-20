47ff9f35fae0dd577979008412265cb443b04357
Tomasz Wojciechowski <t.wojciechowski.648@studms.ug.edu.pl>
2024-06-20 10:10:49 +0200

    frontend initial commit

---
+++ frontend/rnn/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import './styles/hero.css';
+import './styles/howItWorks.css'
+import './styles/model.css';
+import './styles/footer.css';
+
+import Hero from './components/Hero';
+import HowItWorks from './components/HowItWorks';
+import Model from './components/Model';
+import Footer from './components/Footer';
+
+import { Link, Element, scroller } from 'react-scroll';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
